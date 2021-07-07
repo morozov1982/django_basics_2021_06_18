a5fc1752f14aa33eac0e2b9495919b42fe046631
Morozov Alexandr <morozov1982@gmail.com>
2021-07-07 16:25:13 +0600

    add: lesson_5 homework

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render
+
+from basketapp.models import Basket
 from mainapp.models import Product
 
 
 def index(request):
     title = 'магазин'
 
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
     products = Product.objects.all()[:3]
 
     context = {
         'title': title,
         'products': products,
+        'basket': basket,
     }
     return render(request=request, template_name='geekshop/index.html', context=context)
 
 
 def contacts(request):
     title = 'контакты'
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
     context = {
         'title': title,
+        'basket': basket,
     }
     return render(request=request, template_name='geekshop/contact.html', context=context)
